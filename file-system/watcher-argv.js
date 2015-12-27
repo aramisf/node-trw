a2a9bcfc500da3b0d3d4751c6d562a634e3864b0
aramisf <aramis.fernandes@gmail.com>
2015-12-27 17:25:31 -0200

    Adds file existence test to watcher.

--- file-system/watcher-argv.js
+++ file-system/watcher-argv.js
@@ -1,10 +1,16 @@
 const
 fs = require('fs'),
-   filename = process.argv[2];
+filename = process.argv[2];
+
 if (!filename) {
   throw Error("A file to watch must be specified!");
 }
+if (!fs.exists(filename)){
+  console.log("The file '"+filename+"' does not exists.");
+  process.exit(1);
+}
 fs.watch(filename, function() {
     console.log("File " + filename + " just changed!");
-    });
+});
+
 console.log("Now watching " + filename + " for changes...");
