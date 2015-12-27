2dc402b0f297db19163e4557f2079861a1e81c04
aramisf <aramis.fernandes@gmail.com>
2015-12-27 17:40:47 -0200

    Fixes file existence check

--- file-system/watcher-argv.js
+++ file-system/watcher-argv.js
@@ -5,12 +5,15 @@ filename = process.argv[2];
 if (!filename) {
   throw Error("A file to watch must be specified!");
 }
-if (!fs.exists(filename)){
-  console.log("The file '"+filename+"' does not exists.");
-  process.exit(1);
-}
+fs.exists(filename, function(exists) {
+  if (!exists) {
+    console.log("File '"+filename+"' does not exist.");
+    process.exit(1);
+  }
+});
+
 fs.watch(filename, function() {
-    console.log("File " + filename + " just changed!");
+  console.log("File " + filename + " just changed!");
 });
 
 console.log("Now watching " + filename + " for changes...");
