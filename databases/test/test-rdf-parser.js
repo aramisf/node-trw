010d9aba5da32c4277f65f92934336669abca7e7
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2016-11-26 12:11:24 -0200

    Adds some test, using nodeunit.
    nodeunit must be installed with `npm install -g nodeunit`

---
+++ databases/test/test-rdf-parser.js
@@ -0,0 +1,13 @@
+'use strict';
+const
+  rdfParser = require('../lib/rdf-parser.js'),
+  expectedValue = require('./pg132.json');
+
+exports.testRDFParser = (test) => {
+  rdfParser(__dirname + '/pg132.rdf', (err, book) => {
+    test.expect(2);
+    test.ifError(err);
+    test.deepEqual(book, expectedValue, "book should match expected");
+    test.done();
+  });
+};
